f9b4996c21bbda7847a34558682bb91b1d00a1b6
tluu9798 <trungluu9798@gmail.com>
2023-01-19 11:41:41 -0500

    Implement the NoteAdapter and the ViewHolder

--- app/src/main/java/com/alpharettasolutions/notes/view/adapter/NoteAdapter.kt
+++ app/src/main/java/com/alpharettasolutions/notes/view/adapter/NoteAdapter.kt
@@ -1,24 +1,41 @@
 package com.alpharettasolutions.notes.view.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.alpharettasolutions.notes.R
 import com.alpharettasolutions.notes.model.Note
 
 class NoteAdapter(private val noteList: List<Note>): RecyclerView.Adapter<NoteAdapter.ViewHolder>() {
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val titleTextView = itemView.findViewById<TextView>(R.id.tv_note_title)
+        private val contentTextView = itemView.findViewById<TextView>(R.id.tv_note_content)
 
+        fun bind(note: Note) {
+            titleTextView.text = note.title
+            contentTextView.text = note.content
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        return ViewHolder(
+            LayoutInflater
+                .from(parent.context)
+                .inflate(
+                    R.layout.view_holder_note_list,
+                    parent,
+                    false
+                )
+        )
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(noteList[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return noteList.size
     }
 }
