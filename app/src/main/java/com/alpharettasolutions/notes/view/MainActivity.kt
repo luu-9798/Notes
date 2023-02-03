36aaf9b0519dc7fa61dcd318a54bdf741e08d10e
tluu9798 <trungluu9798@gmail.com>
2023-02-03 12:25:16 -0500

    Added ViewModel to MainActivity and the NoteDetailFragment, as well as added DataBinding to NoteDetail

--- app/src/main/java/com/alpharettasolutions/notes/view/MainActivity.kt
+++ app/src/main/java/com/alpharettasolutions/notes/view/MainActivity.kt
@@ -2,11 +2,17 @@ package com.alpharettasolutions.notes.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.alpharettasolutions.notes.R
+import com.alpharettasolutions.notes.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
     }
 }
--- app/src/main/java/com/alpharettasolutions/notes/view/NoteDetailFragment.kt
+++ app/src/main/java/com/alpharettasolutions/notes/view/NoteDetailFragment.kt
@@ -4,16 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.alpharettasolutions.notes.R
+import com.alpharettasolutions.notes.databinding.FragmentNoteDetailBinding
+import com.alpharettasolutions.notes.viewmodel.MainViewModel
 
 class NoteDetailFragment : Fragment() {
+
+    private lateinit var binding: FragmentNoteDetailBinding
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_note_detail, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_note_detail, container, false)
+
+        viewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        return binding.root
     }
 }
